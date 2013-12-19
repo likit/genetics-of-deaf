97c79e9b750a17aaeeb2ef1ddfa5f07672763a5a
Likit Preeyanon <preeyano@msu.edu>
2013-12-18 19:50:14 -0500

    add code to check a given output directory before running blat.

--- run-blat-indel.sh
+++ run-blat-indel.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-# Run BLAT
 genome=$1
 input=$2
 outdir=$3
 
-~/blat -dots=1000 -noHead $genome $input $outdir/$input.psl
-cd $outdir
+# Run BLAT
+test -d $outdir && cd $outdir || exit 1
+~/blat -dots=1000 -noHead $genome $input $input.psl
+
 # Sort and select alignments
 sorted_psl=$(basename $input.psl .psl).sorted.psl
 filtered_psl=$(basename $input.psl .psl).flt.psl
@@ -15,7 +16,7 @@ sort -k 10 $input.psl > $sorted_psl
 pslReps -nohead -noIntrons -singleHit $sorted_psl $filtered_psl info
 rm $input.psl # not needed anymore
 
-printf "Filtering...\n"
 # Filter small indels
+printf "Filtering...\n"
 awk '$7<2' $filtered_psl | awk '$8>10000' > $(basename $filtered_psl .psl).large_ins.psl
 cd -
