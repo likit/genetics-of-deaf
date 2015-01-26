56fe1a3731bbb36ea388891abd03fe1b2c47b98b
Likit Preeyanon <preeyano@msu.edu>
2015-01-26 10:22:25 +0700

    added some useful comments

--- select_vcf_cols.py
+++ select_vcf_cols.py
@@ -7,12 +7,16 @@ def main():
     '''Main function'''
 
     fp = sys.argv[1]  # input file
-    keyword = sys.argv[2]
+    keyword = sys.argv[2] # prefix or etc.
     data = read_csv(fp, sep='\t')
+
+    # if keyword in column names, it will be True
     mask = [keyword in col for col in data.columns.values]
-    # print mask
-    select_data = data.ix[:, mask]
-    select_data.to_csv(sys.stdout, sep='\t')
+    # mask will be like [True, False, True, True..]
+
+    select_data = data.ix[:, mask]  # select columns corresponding 
+                                    # to True value in mask.
+    select_data.to_csv(sys.stdout, sep='\t')  # print results in csv.
 
 
 if __name__=='__main__':
