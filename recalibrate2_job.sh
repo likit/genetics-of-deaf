c3dc2042175a47e5dd6a12620cce6dbf300976ef
Likit Preeyanon <preeyano@msu.edu>
2015-08-04 10:49:06 -0400

    added recalibrate scripts

---
+++ recalibrate2_job.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -login
+#PBS -l nodes=1:ppn=1,mem=24gb,walltime=8:00:00
+#PBS -M preeyano@msu.edu
+#PBS -m abe
+#PBS -N Recalibrate2
+
+module load GATK/3.3.0
+
+cd ${PBS_O_WORKDIR}
+java -Xmx10g -cp $GATK -jar $GATK/GenomeAnalysisTK.jar -T BaseRecalibrator -R hg19.fa -I ${input} -knownSites snpdata/dbsnp.hg19.vcf -BQSR ${input}.table -o ${input}.postcal.table
